8217186b78a6f855292364db7b7823535b253f9c
vrslxowe <vrslxowe@gmail.com>
2024-01-05 16:16:56 +0900

    JDBC Insert 테스트

--- src/com/KoreaIT/java/JDBCAM/test/JDBCInsertTest.java
+++ src/com/KoreaIT/java/JDBCAM/test/JDBCInsertTest.java
@@ -2,11 +2,13 @@ package com.KoreaIT.java.JDBCAM.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class JDBCInsertTest {
 	public static void main(String[] args) {
 		Connection conn = null;
+		PreparedStatement pstmt = null;
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -14,6 +16,20 @@ public class JDBCInsertTest {
 
 			conn = DriverManager.getConnection(url, "root", "");
 			System.out.println("연결 성공!");
+
+			String sql = "INSERT INTO article ";
+			sql += "SET regDate = NOW(),";
+			sql += "updateDate = NOW(),";
+			sql += "title = CONCAT('제목', RAND()),";
+			sql += "`body` = CONCAT('내용', RAND());";
+
+			System.out.println(sql);
+
+			pstmt = conn.prepareStatement(sql);
+
+			int affectedRow = pstmt.executeUpdate();
+
+			System.out.println("affectedRow : " + affectedRow);
 
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패");
@@ -27,31 +43,14 @@ public class JDBCInsertTest {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+			try {
+				if (pstmt != null && !pstmt.isClosed()) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
 }
-
-DROP DATABASE IF EXISTS `JDBC_AM`;
-CREATE DATABASE `JDBC_AM`;
-USE `JDBC_AM`;
-
-CREATE TABLE article(
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
-    title CHAR(100) NOT NULL,
-    `body` TEXT NOT NULL
-);
-
-SHOW TABLES;
-
-SELECT *
-FROM article;
-
-
-INSERT INTO article
-SET regDate = NOW(),
-updateDate = NOW(),
-title = CONCAT('제목', RAND()),
-`body` = CONCAT('내용', RAND());
